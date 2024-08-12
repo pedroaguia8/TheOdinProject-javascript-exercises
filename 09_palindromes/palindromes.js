29b1140f8c032f2a33c57011a99e8b785f486e84
pedroaguia8 <24630163+pedroaguia8@users.noreply.github.com>
2024-08-12 19:14:31 +0100

    Solve exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
 
+// Check solution folder, it is way cleaner
+const palindromes = function (sentence) {
+
+    sentence = sentence.replace(/\s+/g, '');
+    sentence = sentence.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    sentence = sentence.toLowerCase();
+
+    let len = sentence.length;
+    let mid = Math.floor(len/2);
+
+    for (let i = 0; i < mid; i++) {
+        if(sentence[i] !== sentence[len - i - 1])
+            return false;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
