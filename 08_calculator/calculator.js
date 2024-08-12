6a7f9d0829f5a73bb9a28c077b4d5e12775527b8
pedroaguia8 <24630163+pedroaguia8@users.noreply.github.com>
2024-08-12 19:01:41 +0100

    Solve exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+
+  for (const n of array) {
+    sum += n;
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let total = 1;
 
+  for (const n of array) {
+    total *= n;
+  }
+
+  return total;
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return Math.pow(base, exp);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if(n === 0 || n === 1)
+    return 1;
+
+  let total = 1;
+  for (let i = 1; i <= n; i++) {
+    total *= i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
