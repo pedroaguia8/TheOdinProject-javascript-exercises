2d8b0074d32e18dee52ed5cf2e30d814c6579222
pedroaguia8 <24630163+pedroaguia8@users.noreply.github.com>
2024-08-12 10:48:05 +0100

    Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if(num === 0)
+        return "";
+    if(num < 0)
+        return "ERROR";
 
+    let repeatedString = string;
+
+    // i = 1 because if num is 1, the string should
+    // stay the same
+    for (let i = 1; i < num; i++) {
+        repeatedString = repeatedString + string;
+    
+    }
+
+    return repeatedString;
 };
 
 // Do not edit below this line
