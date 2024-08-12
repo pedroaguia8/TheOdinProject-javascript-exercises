e3b84e5b53e18c82cfde70f7bf81ce0f1462f6c5
pedroaguia8 <24630163+pedroaguia8@users.noreply.github.com>
2024-08-12 18:49:45 +0100

    Solve exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degFah) {
+  let degCel = (degFah - 32) * 5 / 9; 
+
+  return Math.round(degCel * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degCel) {
+  let degFah = degCel * 9 / 5 + 32;
+
+  return Math.round(degFah * 10) / 10;
 };
 
 // Do not edit below this line
