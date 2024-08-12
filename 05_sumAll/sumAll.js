b6b3b69b0ed8371568df170a22dc6418bae0ebc1
pedroaguia8 <24630163+pedroaguia8@users.noreply.github.com>
2024-08-12 18:23:02 +0100

    Solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+    if (n1 < 0 || n2 < 0) {
+        return "ERROR";
+    }
+    if (!Number.isInteger(n1) || !Number.isInteger(n2)) {
+        return "ERROR";
+    }
 
+    if (n1 > n2) {
+        let temp = n1;
+        n1 = n2;
+        n2 = temp;
+    }
+
+    let total = 0;
+
+    for (let i = n1; i <= n2; i++) {
+        total += i;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
